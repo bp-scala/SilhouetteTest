04b1ac222f9c8cb4815013f0d57ce259b5d1c8aa
Arpad Tamasi <arpad.tamasi@samebug.io>
2015-07-14 22:45:11 +0200

    controller refactored

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,10 +5,12 @@ import javax.inject.Inject
 
 import com.mohiva.play.silhouette.api._
 import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
+import com.mohiva.play.silhouette.api.services.AuthenticatorResult
 import com.mohiva.play.silhouette.api.util.PasswordHasher
 import com.mohiva.play.silhouette.impl.authenticators.CookieAuthenticator
 import com.mohiva.play.silhouette.impl.providers.CredentialsProvider
 import forms.SignUpForm
+import forms.SignUpForm.Data
 import models.TestUser
 import models.services.TestUserService
 import play.api.i18n.{I18nSupport, Messages, MessagesApi}
@@ -27,7 +29,7 @@ class Application @Inject()(val messagesApi: MessagesApi, passwordHasher: Passwo
     Ok(views.html.secured("Your secured new application is ready."))
   }
 
-  def signUp = UserAwareAction { implicit request =>
+  def signUpPage = UserAwareAction { implicit request =>
     request.identity match {
       case Some(user) => Redirect(routes.Application.index)
       case None => Ok(views.html.signUp(SignUpForm.form))
@@ -39,30 +41,41 @@ class Application @Inject()(val messagesApi: MessagesApi, passwordHasher: Passwo
    *
    * @return The result to display.
    */
-  def postSignUp = Action.async { implicit request =>
+  def signUp = Action.async { implicit request =>
     SignUpForm.form.bindFromRequest.fold(
       form => Future.successful(BadRequest(views.html.signUp(form))),
       data => {
         val loginInfo = LoginInfo(CredentialsProvider.ID, data.email)
         userService.retrieve(loginInfo).flatMap {
-          case Some(user) =>
-            Future.successful(Redirect(routes.Application.signUp()).flashing("error" -> Messages("user.exists")))
-          case None =>
-            val authInfo = passwordHasher.hash(data.password)
-            val user = TestUser(id = UUID.randomUUID(), loginInfo = loginInfo)
-            for {
-              user <- userService.save(user)
-              authInfo <- authInfoRepository.add(loginInfo, authInfo)
-              authenticator <- env.authenticatorService.create(loginInfo)
-              value <- env.authenticatorService.init(authenticator)
-              result <- env.authenticatorService.embed(value, Redirect(routes.Application.index()))
-            } yield {
-              env.eventBus.publish(SignUpEvent(user, request, request2Messages))
-              env.eventBus.publish(LoginEvent(user, request, request2Messages))
-              result
-            }
+          case Some(user) => Future.successful(Redirect(routes.Application.signUpPage()).flashing("error" -> Messages("user.exists")))
+          case None => doSignUp(request, data, loginInfo)
         }
       }
     )
   }
+
+  private def doSignUp(request: Request[AnyContent], data: Data, loginInfo: LoginInfo): Future[AuthenticatorResult] = for {
+    savedUser <- createUser(data, loginInfo)
+    cookie <- createCookie(loginInfo)
+    result <- env.authenticatorService.embed(cookie, Redirect(routes.Application.index()))
+  } yield {
+      env.eventBus.publish(SignUpEvent(savedUser, request, request2Messages))
+      env.eventBus.publish(LoginEvent(savedUser, request, request2Messages))
+      result
+    }
+
+
+  private def createUser(signUpData: Data, loginInfo: LoginInfo): Future[TestUser] = {
+    val authInfo = passwordHasher.hash(signUpData.password)
+    val user = TestUser(id = UUID.randomUUID(), loginInfo = loginInfo)
+    for {
+      savedUser <- userService.save(user)
+      savedPasswordInfo <- authInfoRepository.add(loginInfo, authInfo)
+    } yield savedUser
+  }
+
+  private def createCookie(loginInfo: LoginInfo): Future[Cookie] = for {
+    authenticator <- env.authenticatorService.create(loginInfo)
+    cookie <- env.authenticatorService.init(authenticator)
+  } yield cookie
 }
